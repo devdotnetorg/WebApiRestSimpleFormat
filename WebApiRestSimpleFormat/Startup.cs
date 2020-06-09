b7f0e8941ac6e47d3d11ac2afd1bce9015d9d7e9
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2020-06-10 01:17:56 +0300

    Update

--- WebApiRestSimpleFormat/Startup.cs
+++ WebApiRestSimpleFormat/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace WebApiRestSimpleFormat
@@ -22,7 +16,6 @@ namespace WebApiRestSimpleFormat
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -33,7 +26,6 @@ namespace WebApiRestSimpleFormat
              
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,16 +35,13 @@ namespace WebApiRestSimpleFormat
 
             // Enable middleware to serve generated Swagger as a JSON endpoint
             app.UseSwagger();
-
             // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-
             app.UseRouting();
-
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
